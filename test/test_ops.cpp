81496c02dcd9994af0892dbf85f360efad38f439
Nick Jones <nick.fa.jones@gmail.com>
2016-02-26 06:36:39 +0000

    test_case_extend1

--- test/test_ops.cpp
+++ test/test_ops.cpp
@@ -695,6 +695,57 @@ const char *test_case_trim1::output = "abcdefghijklmnop";
 
 /*******************************************************************************
  */
+class test_case_extend1 : public test_case<test_case_extend1> {
+  public:
+    static const char *input;
+    static const char *output;
+
+  public:
+    virtual int run_test(const test_subject& subject) {
+      subject.buffer->clear();
+
+      TEST_OPS_EVAL(subject.buffer->get_data_size() != 0)
+        return 1;
+
+      if (subject.buffer->get_strategy().rejects_extend)
+        return 0;
+
+      TEST_OPS_EVAL(subject.buffer->write(input, 13) != 13)
+        return 1;
+
+      TEST_OPS_EVAL(subject.buffer->get_data_size() != 13)
+        return 1;
+
+      TEST_OPS_EVAL(subject.buffer->extend(13) != 13)
+        return 1;
+
+      TEST_OPS_EVAL(subject.buffer->get_data_size() != 26)
+        return 1;
+
+      TEST_OPS_EVAL(subject.buffer->overwrite(
+            input, strlen(input)) != strlen(input))
+        return 1;
+
+      pb::buffer::byte_iterator byte_itr = subject.buffer->byte_begin();
+
+      for (unsigned int i = 0; i < strlen(output); ++i) {
+        TEST_OPS_EVAL(*byte_itr != output[i])
+          return 1;
+
+        ++byte_itr;
+      }
+
+      return 0;
+    }
+};
+
+const char *test_case_extend1::input = "abcdefghijklmnopqrstuvwxyz";
+const char *test_case_extend1::output = "abcdefghijklmnopqrstuvwxyz";
+
+
+
+/*******************************************************************************
+ */
 int main(int argc, char **argv) {
   std::list<test_subject> test_subjects;
 
@@ -777,6 +828,7 @@ int main(int argc, char **argv) {
   test_case<test_case_rewind1>::run_test(test_subjects);
   test_case<test_case_rewind2>::run_test(test_subjects);
   test_case<test_case_trim1>::run_test(test_subjects);
+  test_case<test_case_extend1>::run_test(test_subjects);
 
   test_subjects.clear();
 
