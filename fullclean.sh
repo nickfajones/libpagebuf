ea5e8d22c1b48e16e9f7fd3fd1f87cfd27648fdd
Nick Jones <nick.fa.jones@gmail.com>
2017-08-04 21:44:01 +0800

    Update fullclean script

--- fullclean.sh
+++ fullclean.sh
@@ -1,6 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-for a in `cat .gitignore`
-do
-  rm -rf ./$a
+homedir=$PWD
+
+for dir in . pagebuf test; do
+  cd $dir
+  if [ ! -e .gitignore ]; then
+    cd $homedir
+    continue
+  fi
+  while read -r line; do
+    if [[ "$line" =~ ^# ]]; then
+      continue
+    fi
+    if [[ -z "$line" ]]; then
+      continue
+    fi
+    rm -rf ./$line
+  done <.gitignore
+  cd $homedir
 done
